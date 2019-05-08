fa009cb3988465dd0be270d530d9a2169e5eb7d1
Marcelo Moraes <marcelo.moraes@geru.com.br>
2019-05-08 16:42:17 -0300

    Up version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md') as f:
 
 setup(
     name='geru.marshmallow_jsonapi',
-    version='0.1.0',
+    version='0.1.2',
     description='Marshmallow JsonAPI',
     long_description=long_description,
     long_description_content_type='text/markdown',
