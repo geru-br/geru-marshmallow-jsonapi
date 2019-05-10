bdeefd8ec09fb44d8b9b16b689de3b0eb196adef
Marcelo Moraes <marcelo.moraes@geru.com.br>
2019-05-10 15:54:53 -0300

    Written more test and fix them

--- tests/test_apificator.py
+++ tests/test_apificator.py
@@ -7,6 +7,11 @@ def must_be_book(data):
         raise ValidationError('type must be book')
 
 
+def must_be_author(data):
+    if data != 'author':
+        raise ValidationError('type must be author')
+
+
 @JsonApificator()
 class BookSchemaDefault(Schema):
     title = fields.Str()
@@ -27,6 +32,27 @@ class BookSchemaCustomValidator(Schema):
     title = fields.Str()
 
 
+class PublishSchema(Schema):
+    name = fields.Str()
+
+
+class AuthorSchema(Schema):
+    name = fields.Str()
+
+
+@JsonApificator(type_={"validate": must_be_book, "required": True}, relationship=[{"relationship": AuthorSchema,
+                                                                                   "extra_kwargs": {"required": True}}])
+class BookSchemaRelationship(Schema):
+    title = fields.Str()
+
+
+@JsonApificator(type_={"validate": must_be_book, "required": True}, relationship=[{"relationship": AuthorSchema,
+                                                                                   "extra_kwargs": {"required": True}},
+                                                                                  {"relationship": PublishSchema}])
+class BookSchemaWithTwoRelationship(Schema):
+    title = fields.Str()
+
+
 class TestApificator:
     def test_default(self):
         _payload = {}
@@ -35,30 +61,101 @@ class TestApificator:
     def test_type_required(self):
         _payload = {}
         book = BookSchemaTypeRequired().load(_payload)
-        assert book.errors['type'] == ['Missing data for required field.']
-        _payload = {"type": "book"}
+        assert book.errors['data']['type'] == ['Missing data for required field.']
+        _payload = {
+            "data": {
+                "attributes": {},
+                "type": "book"
+            }
+        }
         book = BookSchemaTypeRequired().load(_payload)
         assert book.errors == {}
 
     def test_everyone_required(self):
-        _payload = {}
+        _payload = {
+            "data": {}
+        }
         book = BookSchemaEveryoneRequired().load(_payload)
-        assert book.errors['type'] == ['Missing data for required field.']
-        assert book.errors['id'] == ['Missing data for required field.']
-        assert book.errors['attributes'] == ['Missing data for required field.']
-        _payload = {"type": "book",
-                    "attributes": {},
-                    "id": ''}
+        assert book.errors['data']['type'] == ['Missing data for required field.']
+        assert book.errors['data']['id'] == ['Missing data for required field.']
+        assert book.errors['data']['attributes'] == ['Missing data for required field.']
+        _payload = {
+            "data": {
+                "type": "book",
+                "attributes": {},
+                "id": ""
+            }}
         book = BookSchemaEveryoneRequired().load(_payload)
         assert book.errors == {}
 
     def test_custom_validator(self):
         _payload = {}
         book = BookSchemaCustomValidator().load(_payload)
-        assert book.errors['type'] == ['Missing data for required field.']
-        _payload = {"type": "books"}
+        assert book.errors['data']['type'] == ['Missing data for required field.']
+        _payload = {
+            "data": {
+                "type": "books"
+            }
+        }
         book = BookSchemaCustomValidator().load(_payload)
-        assert book.errors['type'] == ['type must be book']
-        _payload = {"type": "book"}
+        assert book.errors['data']['type'] == ['type must be book']
+        _payload = {
+            "data": {
+                "type": "book"
+            }
+        }
         book = BookSchemaCustomValidator().load(_payload)
         assert book.errors == {}
+
+    def test_one_relationship(self):
+        _payload = {
+            "data": {
+                "relationship": {}
+            }
+        }
+        book = BookSchemaRelationship().load(_payload)
+        assert book.errors['data']['relationship']
+        assert book.errors['data']['type']
+        _payload = {
+            "data": {
+                "type": "book",
+                "attributes": {
+                    "title": "It"
+                },
+                "relationship": {
+                    "author_schema": {
+                        "name": "Jose"
+                    }
+                }
+            }
+        }
+        book = BookSchemaRelationship().load(_payload)
+        assert not book.errors
+
+    def test_two_relationship(self):
+        _payload = {
+            "data": {
+                "relationship": {}
+            }
+        }
+        book = BookSchemaWithTwoRelationship().load(_payload)
+        assert len(book.errors['data']['relationship']) == 1
+        assert book.errors['data']['type']
+        _payload = {
+            "data": {
+                "type": "book",
+                "attributes": {
+                    "title": "It"
+                },
+                "relationship": {
+                    "author_schema": {
+                        "name": "Jose"
+                    },
+                    "publish_schema": {
+                        "name": "Martin"
+                    }
+                }
+            }
+        }
+        book = BookSchemaWithTwoRelationship().load(_payload)
+        assert not book.errors
